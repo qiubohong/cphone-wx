579851f6d7c42115fe0b928a10801bc082918dc1
qiubohong <550766314@qq.com>
2017-12-19 23:17:07 +0800

    微信支付bug修复

--- src/utils/index.js
+++ src/utils/index.js
@@ -83,7 +83,7 @@ export function wxPay(appId, timeStamp, nonceStr, package2, signType, paySign) {
         "paySign": paySign //微信签名 
     },function(res) {
         // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
-        if (res.err_msg == "get_brand_wcpay_request：ok") {
+        if (res.err_msg == "get_brand_wcpay_request:ok") {
           resolve(res);
         }else{
           reject("微信支付失败");
